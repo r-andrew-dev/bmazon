9ece918bfb04717789619f095f818cdd0af3606d
Rachael Andrew <r.andrew.dev@gmail.com>
2020-02-26 23:09:02 -0600

    running through prompt appropriately, requesting correct info from customer, need to adjust a few variables still

--- bmazonCustomer.js
+++ bmazonCustomer.js
@@ -3,12 +3,20 @@ const inquirer = require('inquirer');
 const inqConfirm = require('inquirer-confirm')
 
 const connection = mysql.createConnection({
-  host: "localhost",port: 3306,user: "root",password: "Midnight*1",database: "bmazon"
+  host: "localhost", port: 3306, user: "root", password: "Midnight*1", database: "bmazon"
 });
 
-const confirm = function() {inqConfirm('Would you like to make a purchase?')
-  .then(function questions() {
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  displayProducts();
+});
 
+const confirm = function () {
+  inqConfirm('Would you like to make a purchase?')
+  .then(function questions() {
+    connection.query("SELECT * FROM products", function(err, results) {
+      if (err) throw err;
     inquirer.prompt([
       {
         type: 'input',
@@ -17,12 +25,11 @@ const confirm = function() {inqConfirm('Would you like to make a purchase?')
         validate: function (value) {
           // regEx exp to validate input is a 1 - 3 digit number. Would need to be modified if over 999 products in database.
           let pass = value.match(/^\d{1,3}$/);
-  
+
           if (pass) {
             return true;
           }
           return 'Please enter a valid product ID number up to 999 using digits 0-9.'
-  
         }
       },
       {
@@ -33,46 +40,62 @@ const confirm = function() {inqConfirm('Would you like to make a purchase?')
           // regEx exp to validate input is a 1-3 digit number. Would need to be updated if 
           // any product had more than 999 in stock. 
           let pass = value.match(/^\d{1,3}$/);
-  
+
           if (pass) {
             return true;
           }
           return 'Please enter a valid product quantity up to 999 as a number using digits 0-9.'
         }
-  
+
       },
-    ]
-    )
-    
-  }, function cancelled() {
-    console.log('Sorry to hear that. Thanks for stopping by!')
-    connection.end()
+    ]).then(answers => {
+        let chosenProduct;
+        for (let i = 0; i < results.length; i++) {
+          if (results[i].id === answers.id) {
+            chosenProduct = results[i];
+          }
+        }
+
+        if (chosenProduct.stock_quantity <= parseInt(answer.quantity)) {
+          connection.query(
+            "UPDATE products SET ? WHERE ?", [
+            {
+              stock_quantity: (products.stock_quantity - answer.quantity)
+            },
+            {
+              id: chosenProduct
+            }
+          ],
+            function (error) {
+              if (error) throw err;
+              console.log("Order placed successfully! Your total is: " + (parseInt(chosenProduct.stock_quantity) * parseInt(chosenProduct.price)));
+              displayProducts();
+            });
+        }
+
+        else {
+
+          console.log("I'm sorry. Your product ID was not available in our database or the quantity requested was too high")
+          displayProducts();
+        }
+      });
+
+      })}, function cancelled() {
+        console.log('Sorry to hear that. Thanks for stopping by!')
+        connection.end()
+      })
+  };
+
+  function displayProducts() {
+    console.log("Welcome to the store! Fetching available products...\n");
+    connection.query("SELECT * FROM products", function (err, res) {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      for (let i = 0; i < res.length; i++) {
+        console.log("ID: " + res[i].id + " | Product: " + res[i].product_name + " | Price: $" + res[i].price + ".00 | Available: " + res[i].stock_quantity +"\n");
+      }
+      console.log("Welcome to the store! Please scroll up to see all available products. ");
+      confirm()
+    });
   }
-  )};
 
-connection.connect(function (err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-  displayProducts();
-});
-
-function displayProducts() {
-  console.log("Welcome to the store! Fetching available products...\n");
-  connection.query("SELECT * FROM products", function (err, res) {
-    if (err) throw err;
-    // Log all results of the SELECT statement
-    for (let i = 0; i < res.length; i++) {
-      console.log("ID: " + res[i].id + " | Product: " + res[i].product_name + " | Price: $" + res[i].price + ".00 |\n");
-    }
-    console.log("Welcome to the store! Please scroll up to see all available products. ");
-    confirm()
-  });
-}
-
-// function askCustomer() {
-
-//   inquirer.prompt(questions)
-//     .then(answers => {
-//       console.log(JSON.stringify(answers, null, ' '));
-//     })
-// }
