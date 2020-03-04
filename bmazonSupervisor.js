87e6310887c0f65da25804b678ca65e2a5e7e073
Rachael Andrew <r.andrew.dev@gmail.com>
2020-03-03 21:59:17 -0600

    adding departments correctly for bmazonSupervisor, troubleshooting why new departments are not showing up in terminal but show in sql

--- bmazonSupervisor.js
+++ bmazonSupervisor.js
@@ -37,8 +37,8 @@ function displayOptions() {
             case 'Create New Department':
                 createNewDepartment()
                 break;
-            default: 
-            cancelled();
+            default:
+                cancelled();
         }
     })
 }
@@ -48,9 +48,53 @@ function viewProductSales() {
     query += "SUM(p.product_sales) AS dept_sales, SUM(p.product_sales) - over_head_costs AS total_profit FROM products p "
     query += "JOIN departments d ON p.department_name = d.department_name "
     query += "GROUP BY d.dept_id, d.department_name ORDER BY total_profit DESC;"
-    connection.query(query, function(err, results) {
+    connection.query(query, function (err, results) {
         if (err) throw err;
         console.table(results);
-        displayOptions ();
+        displayOptions();
     })
+}
+
+function createNewDepartment() {
+    connection.query("SELECT * FROM departments", function (err, results) {
+        if (err) throw err
+        prompt([
+            {
+                type: 'input',
+                name: 'departmentName',
+                message: 'Please enter the name for the department to create.',
+                validate: function (value) {
+                    if (isNaN(value) === true) {
+                        return true;
+                    }
+                    return 'Please enter a valid department name.';
+                }
+            },
+            {
+                type: 'number',
+                name: 'overHeadCosts',
+                message: 'What is the overhead cost for this department?',
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return 'Please enter a valid overhead cost using digits 0-9.';
+                }
+            },]).then(function (answer) {
+
+                let newDepartment = answer.departmentName
+                let newOverHead = answer.overHeadCosts
+                connection.query(
+                    "INSERT INTO departments SET ?",
+                    {
+                        department_name: newDepartment,
+                        over_head_costs: newOverHead,
+                    },
+                    function (err, results) {
+                        if (err) throw err;
+                        console.log("Department " + newDepartment + " has been added sucessfully with over head cost of $" + newOverHead + ".") 
+                    })
+            })
+    })
+
 }
